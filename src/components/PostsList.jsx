371b592ed78eb4253ff1f42e6bc736cc5bfdbe6b
shivrajpro <shivrajhulbatte@gmail.com>
2023-11-07 00:22:52 +0530

    working with state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css"
 
 function PostsLIst(props) {
   return (
+    <>
+    <NewPost/>    
     <ul className={classes.posts} >
       <Post author="Shivraj" body="React.js is awsome" />
       <Post author="Maxmilian" body="But I love Angular!" />
     </ul>
+    </>
   );
 }
 
